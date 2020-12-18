56a358518fac2008d181692cd4ae596e3fcb70a9
aelguetac <alejandro.elgueta@gmail.com>
2020-12-17 21:30:04 -0300

    se agrega build gradle-maven 3

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,6 +7,9 @@ pipeline {
         	stage('Pipeline') {
             		steps {
                 		script {
+					env.TAREA = ''
+					def cadena = "hola ${params.buildtool}"
+					def cadena = "{env.TAREA}"
 					echo params.herramienta
 					if (params.herramienta == 'gradle' ){
 					        def ejecucion = load 'gradle.groovy'
@@ -21,6 +24,22 @@ pipeline {
                         }
 			}
 		}
+		post{
+
+			sucess {
+				println env.TAREA
+				println env.JOB_NAME
+				println params.buildtool
+				def mensaje = "{env.TAREA} "
+				slackSend color: 'good', message: '[Alejandro Elgueta][JOB_NAME]', teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack'
+			}
+			failure {
+                                println env.TAREA." "env.JOB_NAME
+				slackSend color: 'danger', message: '[Alejandro Elgueta][JOB_NAME]', teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack'				
+
+			}
+		}
+
 	}
 }
 
--- gradle.groovy
+++ gradle.groovy
@@ -8,11 +8,13 @@
 def call(){
 
                         stage('build test') {
+			env.TAREA = 'build test'
                         sh "./gradlew clean build"
 
 
                         }
                         stage('sonar') {
+			env.TAREA = 'sonar'
                         def scannerHome = tool 'sonar_scanner';
 
                         withSonarQubeEnv('sonar') {
@@ -23,18 +25,21 @@ def call(){
 
                         }
                         stage('Run'){
+			env.TAREA = env.STAGE_NAME
                         //sh 'nohup gradlew bootrun &'
                         bat "start gradlew bootRun"
 
                         }
 
-                         stage('Test'){i
+                        stage('Test'){
+			env.TAREA = env.STAGE_NAME
                         sh "curl -X GET 'http://localhost:8087/rest/mscovid/test?msg=testing'"
 
                         }
 
                         stage('Nexus'){
-                                        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus',
+			env.TAREA = env.STAGE_NAME
+                        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus',
                 packages: [[ $class: 'MavenPackage', MavenAssetList: [[classifier: 'RELEASE', extensions: 'jar' ,
                 filePath: './build/libs/DevOpsUsach2020-0.0.1.jar']],
                 mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging : 'jar', version: '0.0.1']]]
