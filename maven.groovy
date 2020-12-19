62b351281098ce2d59ebf0635d7311188279e2ae
aelguetac <alejandro.elgueta@gmail.com>
2020-12-19 00:05:29 -0300

    se agrega build gradle-maven url 7

--- maven.groovy
+++ maven.groovy
@@ -42,7 +42,7 @@ def call(){
 			env.TAREA = env.STAGE_NAME
                         nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus',
                 packages: [[ $class: 'MavenPackage', MavenAssetList: [[classifier: 'RELEASE', extensions: 'jar' ,
-                filePath: './build/libs/DevOpsUsach2020-0.0.1.jar']],
+                filePath: './nada/build/libs/DevOpsUsach2020-0.0.1.jar']],
                 mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging : 'jar', version: '0.0.1']]]
 
 
